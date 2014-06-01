076f5ef99045da70b0d060eb573ed6e810dc8981
Caleb Smith <belac1000@gmail.com>
2014-06-01 18:40:19 -0400

    Made test.py draw hexagons instead of Triangles

--- test.py
+++ test.py
@@ -1,10 +1,21 @@
 from fredx import *
-begin()
-loops(100,hexagon)
-loops(100,circle)
+from msvcrt import getch
 
-i = 100
-while i <= 350:
-    loops(i,triangle)
-    i = i+25
-loops(175,circle)
+def fireItUp():
+    loops(100,hexagon)
+    loops(100,circle)    
+    i = 100
+    while i <= 200:
+        loops(i,triangle)
+        i = i+25
+    i = 200
+    while i <= 300:
+        loops(i,hexagon)
+        i = i+25
+    loops(250,circle)
+
+begin()
+if raw_input("Press G when ready") == "g" or "G":
+    fireItUp()
+
+
